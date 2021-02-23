029877e79110b56082f46e93f94ce64b98e38203
Erik Higginbotham <erik96@me.com>
2021-02-23 08:47:13 -0500

    initial commit

---
+++ src/pages/projects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Projects() {
+    return (
+        <div style={{ fontSize: "20px" }}>
+            Projects coming soon..
+        </div>
+    );
+}
