6ea72084a66e79c3e99aaa850581a457110654c7
Erik Higginbotham <higgi615@ufl.edu>
2021-02-23 20:48:07 -0500

    Updates

--- src/pages/projects.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Projects() {
-    return (
-        <div style={{ fontSize: "20px" }}>
-            Projects coming soon..
-        </div>
-    );
-}
