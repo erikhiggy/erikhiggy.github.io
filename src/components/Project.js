2351bc0a9d51fed1085ff12191974f1746b83dd2
Erik Higginbotham <higgi615@ufl.edu>
2021-04-13 20:59:21 -0400

    add basic projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,19 +1,28 @@
 import React from 'react';
 
 export default function Project({ data }) {
-  const { description, title, image, link } = data;
+  const { description, title, links, tech } = data;
+  const renderLinks = ({ links }) => {
+    if (links.length > 1) {
+      return (
+        <>
+          <a style={{ paddingRight: 5 }} href={links[0]}>Frontend Code</a>
+          <a href={links[1]}>Backend Code</a>
+        </>
+      )
+    }
+    return (
+      <a href={links[0]}>Code</a>
+    )
+  };
   return (
-    <div className="project-container">
-      <div className="col one-third px2">
-        {image}
-      </div>
-      <div className="col two-third px2">
-        <a href={link} className="no-decor">
-          <h2 className="project-title">
-            {title}
-          </h2>
-        </a>
-      </div>
+    <div className="col two-third px2 project-container">
+      <h2 className="project-title">
+        {title}
+      </h2>
+      <p>Tech Stack - {tech}</p>
+      <p>{description}</p>
+      {renderLinks({ links })}
     </div>
   );
 }
